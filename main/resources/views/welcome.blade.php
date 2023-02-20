80571da4b8db166635f97c5755c97319c9d54bb1
digitalspacedsd <digitalspacedsd@gmail.com>
2023-02-20 16:45:09 -0500

    working

--- main/resources/views/welcome.blade.php
+++ main/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
 @section('cabecera')
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-        <h1 class="display-4 fw-normal">Inicio.</h1>
+        <h1 class="display-4 fw-normal">Inicio</h1>
         <p class="fs-5 text-muted">Aqui podras encontrar un CRUD con dos tipos de roles que son administrador y usuario. Los
             usuarios con rol Administrador podran realizar cambios en el aplicativo mientras que los que son de rol
             'usuario' solo podran ver informacion.</p>
