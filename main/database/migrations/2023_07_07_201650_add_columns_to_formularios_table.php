54d68ceb689b80e6f01d65754f0352d50f44650c
MiguelFPP <digitalspacedsd@gmail.com>
2023-07-07 15:33:54 -0500

    doctrine/dbal

---
+++ main/database/migrations/2023_07_07_201650_add_columns_to_formularios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToFormulariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('formularios', function (Blueprint $table) {
+            $table->string('vinculo')->nullable();
+            $table->boolean('estado')->default(true);
+
+            $table->unsignedBigInteger('candidato_id')->nullable()->change();
+            $table->string('zona')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('formularios', function (Blueprint $table) {
+            $table->dropColumn('vinculo');
+            $table->dropColumn('estado');
+
+            $table->unsignedBigInteger('candidato_id')->change();
+            $table->string('zona')->change();
+        });
+    }
+}
