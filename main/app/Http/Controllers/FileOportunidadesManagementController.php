5bd956560e2bd1a4769fb6bb22ad9d570da881d8
Miguel Perdomo <miguelferneyp@gmail.com>
2023-10-17 10:03:47 -0500

    exportar posibles votantes

--- main/app/Http/Controllers/FileOportunidadesManagementController.php
+++ main/app/Http/Controllers/FileOportunidadesManagementController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Exports\Oportunidades\ExportService;
+use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class FileOportunidadesManagementController extends Controller
 {
-    public function export()
+    /**
+     * Export the data to an Excel file.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function export(Request $request)
     {
-        return Excel::download(new ExportService, 'posibles_votantes_'.now().'.xlsx');
+        return Excel::download(new ExportService($request), 'posibles_votantes_'.now().'.xlsx');
     }
 }
